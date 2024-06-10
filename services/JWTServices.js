92f15fe2e21f9626e94a0a361dffea3bea111850
Pasindu9225 <Pasindu9225@gmail.com>
2024-06-10 13:33:40 +0530

    ADD ACCESS_TOKEN_SECRET

--- services/JWTServices.js
+++ services/JWTServices.js
@@ -1,3 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const ACCESS_TOKEN_SECRET =
+  "648a00da769bd71fe0ad2d52e6ffbc3c6ba048f05420392d0ef3ba2a73333711ca2797472859d5f801939ff7960864ef2bf0b16c4578022571e3ff1736f3e0c0";
+
+
 class JWTServices {
   signAccesstoken(payload, expiryTime, secret) {
     return jwt.sign(payload, secret, { expiresIn: expiryTime });
